06b01b96a0ea2ca42a480a08c205afad987f5c7a
jeissongarciacano <47406585+jeissongarciacano@users.noreply.github.com>
2019-08-01 14:17:35 -0500

    Create analizador.js

---
+++ analizador.js
@@ -0,0 +1,23 @@
+// JavaScript source code
+class tipos {
+    constructor(cont, tipo, cualesson) {
+        this.cont = cont;
+        this.tipo = tipo;
+        this.cualesson = cualesson;
+    }
+    vertipos(){
+        return 'los ' + this.tipo + ' son ' + this.cont + ' ' + this.cualesson;
+    }
+}
+class analis {
+    token = ['=', '(', ')', '+', '-', '*', '/', ';', '<', '>'];
+    constructor(){
+    }
+    leer() {
+        res = '';
+        longitud = 0;
+        ch;
+        ind= new BufferedReader();
+    }
+
+}
