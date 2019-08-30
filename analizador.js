a5d2aa59ce95d1f320bc411bc767f886b48c5759
jeissongarciacano <47406585+jeissongarciacano@users.noreply.github.com>
2019-08-30 15:20:08 -0500

    Update analizador.js

--- analizador.js
+++ analizador.js
@@ -73,22 +73,9 @@ class analis {
     a39= new tab(this.ab8,')', 'parentDer' );
     a40= new tab(this.ab8,'{', 'corcheteIzq' );
     a41= new tab(this.ab8,'}', 'corcheteDer');
-   
     lexemas = [this.a1,this.a2,this.a3,this.a4,this.a5,this.a6,this.a7,this.a8,this.a9, this.a10, this.a11,this.a12, this.a13, this.a14,this.a15, this.a16,this.a17,this.a18,this.a19,
      this.a20,this.a21,this.a22,this.a23,this.a24,this.a26,this.a27,this.a28,this.a29,this.a30,this.a31,this.a32,this.a33,this.a35,this.a36,this.a37,this.a38,this.a39,
     this.a40,this.a41];
-    comentarios2 = ['comentariolinea', 'iniciocomentario', 'fincomentario']; 
-    comentarios = ['//', '/*', '*/'];
-    palabrasclaves = ['while', 'for', 'switch', 'var', 'if', 'else', 'try', 'return', 'class', 'throws', 'throw', 'funtion',
-     'super', 'new', 'import', 'do', 'finally', 'false', 'true', 'this'];
-    palabrasclaves2 = ['mientras', 'ciclofor', 'switch', 'var', 'si', 'sino', 'tratar', 'devolver', 'clase', 'throws', 'throw', 'funtion',
-     'super', 'nuevo', 'importar', 'hacer', 'final', 'false', 'true', 'this'];
-    identificadores = ['=', '+', '-', '*', '/',  '<', '>'];
-    identificadores2 = ['igual', 'suma', 'menos', 'mult', 'backslash', 'menorque', 'mayorque'];
-    tipodato = ['var', 'let', 'long'];
-    tipodato2 = ['variable', 'numero', 'numerolong'];
-    separador = [ '(', ')', ';','{', '}' , ','];
-    separador2 = [ 'parentIzq', 'parenteDer', 'puntocoma','corcheteIzq', 'corcheDer' , 'coma' ];
     constructor(){
         this.data= new Array();
     }   
@@ -101,7 +88,6 @@ class analis {
         document.writeln("<td>" + 'tipo' + "</td>");
         document.writeln("<td>" + 'renglon' + "</td>");
         document.writeln("<td>" + 'columna' + "</td>");
-        var aux32= 1;
         for (let index = 0; index < this.data.length; index++) {
             document.write("<tr>");
             document.writeln("<td>" + this.data[index].cont + "</td>");
@@ -111,120 +97,22 @@ class analis {
             document.writeln("<td>" + this.data[index].reng + "</td>");
             document.writeln("<td>" + this.data[index].columna + "</td>");
             document.write("</tr>");
-            aux32=aux32+1;;
         }
-    }
-    // verificadores de tipo, devuelven un booleano y se ingresa una palabra que se compara con los 
-    // arreglos iniciales
-    estipodato(palabra){
-        var r = false;
-        for (let index = 0; index < this.tipodato.length; index++) {
-            if(palabra == this.tipodato[index]){
-                r=true;
-            }
-        }
-        return r;
-    }
-    esseparador(palabra){
-        var r = false;
-        for (let index = 0; index < this.separador.length; index++) {
-            if(palabra == this.separador[index]){
-                r=true;
-            }
-        }
-        return r;
-    }
-    escomentario(palabra){
-        var r = false;
-        for (let index = 0; index < this.comentarios.length; index++) {
-            if(palabra == this.comentarios[index]){
-                r=true;
-            }
-        }
-        return r;
-    }
-    espalabraclave(palabra){
-        var r = false;
-        for (let index = 0; index < this.palabrasclaves.length; index++) {
-            if(palabra == this.palabrasclaves[index]){
-                r=true;
-            }
-        }
-        return r;
-    }
-    esidentificador(palabra){
-        var r = false;
-        for (let index = 0; index < this.identificadores.length; index++) {
-            if(palabra == this.identificadores[index]){
-                r=true;
-            }
-        }
-        return r;
-    }
-    //busca el token y lo asigna
-    bustok(palabra, nume){
-        var tok = palabra;
-        switch (nume) {
-            case 1:
-                for (let index = 0; index < this.comentarios.length; index++){
-                    if(palabra == this.comentarios[index]){
-                        tok = this.comentarios2[index];
-                        }
-                    }  
-                break;
-            case 2:
-                for (let index = 0; index < this.identificadores.length; index++){
-                    if(palabra == this.identificadores[index]){
-                        tok = this.identificadores2[index];
-                        }
-                    }
-                break;
-            case 3:
-                for (let index = 0; index < this.palabrasclaves.length; index++){
-                   if(palabra == this.palabrasclaves[index]){
-                        tok = this.palabrasclaves2[index];
-                        }
-                    }
-                break;
-            case 4:
-                    for (let index = 0; index < this.separador.length; index++){
-                        if(palabra == this.separador[index]){
-                        tok = this.separador2[index];
-                        }
-                    }
-                break;
-            default:
-                for (let index = 0; index < this.tipodato.length; index++){
-                    if(palabra == this.tipodato[index]){
-                        tok = this.tipodato2[index];
-                        }
-                    }
-                break;
-        } 
-        return tok;
     }
     // ejecuta los verificadores anteriores y crea el objeto que ingresa al arreglo data y se almacenan ahi
-    buscadortipo(palabra, rengaux, contaux, col){
-        if(this.escomentario(palabra)){
-                var x= new tipos('comentario', palabra, contaux, rengaux, col, this.bustok(palabra, 1));
+  crear(palabra, rengaux, contaux, col){
+        var x=null;
+        var y= ['variable'];
+        for (let i = 0; i < this.lexemas.length; i++) {
+            if (this.lexemas[i].lexema== palabra) {
+                x= new tipos(this.lexemas[i].tipodato, palabra, contaux, rengaux, col, this.lexemas[i].token);
+            }    
         }
-        else if(this.esidentificador(palabra)){
-                var x= new tipos('identificador', palabra, contaux, rengaux, col,this.bustok(palabra, 2));
+        if(x==null){
+            x= new tipos('variable', palabra, contaux, rengaux, col, palabra);
         }
-        else if(this.espalabraclave(palabra)){
-                var x= new tipos('palabra reservada', palabra, contaux, rengaux, col, this.bustok(palabra, 3));
-        }
-        else if(this.esseparador(palabra)){
-            var x= new tipos('separador', palabra, contaux, rengaux, col,this.bustok(palabra, 4));
-        }
-        else if(this.estipodato(palabra)){
-            var x= new tipos('tipo de dato', palabra, contaux, rengaux, col, this.bustok(palabra, 5));
-        }
-        else{
-                var x= new tipos('variable', palabra, contaux, rengaux, col, palabra);
-            }
-            this.data.push(x);
-        }
+        this.data.push(x);
+    }
     // lee la cadena ingresada y ejecuta el resto del codigo
     lectorcad(cad){
         var arrayaux = cad.split(' ');
@@ -232,8 +120,7 @@ class analis {
         var contaux = 1;
         var colaux= 1;
         for (let index = 0; index < arrayaux.length; index++) {
-
-            this.buscadortipo(arrayaux[index],auxreng, contaux, colaux);
+            this.crear(arrayaux[index], auxreng,contaux, colaux);
             contaux = contaux+1;
             if(arrayaux[index] == ';' || arrayaux[index] == '{' || arrayaux[index] == '}' ){
                 auxreng= auxreng+1;
@@ -242,7 +129,6 @@ class analis {
             else{
                 colaux = colaux+1;
             }
-
         }
     }
    
